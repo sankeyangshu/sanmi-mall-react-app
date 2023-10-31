246488122f9cd60d16f1c2a71184555b8c5c40cf
sankeyangshu <1260323835@qq.com>
2023-10-31 08:44:47 +0800

    style: 代码格式修复

--- src/views/Home/components/GoodsPreview/index.tsx
+++ src/views/Home/components/GoodsPreview/index.tsx
@@ -9,11 +9,18 @@ const GoodsPreview = () => {
       <div className={mCss.content}>
         <div className={mCss.title}>商品标题</div>
         <div className={mCss.tags}>
-          <Tag type="success">标签</Tag>
-          <Tag type="success">标签</Tag>
+          <Tag color="#dcebe6" textColor="#157658" style={{ marginRight: '3px' }}>
+            标签
+          </Tag>
+          <Tag color="#dcebe6" textColor="#157658">
+            标签
+          </Tag>
+        </div>
+        <div className={`${mCss.price} ${mCss.discount}`}>
+          <span>¥</span>8000
         </div>
         <div className={mCss.price}>
-          <span>¥</span>8000
+          <span>¥</span>800
         </div>
         <div className={mCss.subtitle}>
           这是简介呵呵哈哈哈这是简介呵呵哈哈哈这是简介呵呵哈哈哈这是简介呵呵哈哈哈
